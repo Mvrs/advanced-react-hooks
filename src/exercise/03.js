e550750fb5d65328803a16804045c134680cab96
Marlon Johnson <johnsonmarlon18@gmail.com>
2021-03-27 15:13:16 -0700

    completed exercise 03 ec1

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -6,10 +6,10 @@ import * as React from 'react'
 // 🐨 create your CountContext here with React.createContext
 const CountContext = React.createContext()
 
-const CountProvider = ({children}) => {
+const CountProvider = children => {
   const [count, setCount] = React.useState(0)
-  const value = {count, setCount}
-  return <CountContext.Provider value={value}>{children}</CountContext.Provider>
+  const value = [count, setCount]
+  return <CountContext.Provider value={value} {...children} />
 }
 
 // 🐨 create a CountProvider component here that does this:
@@ -18,15 +18,23 @@ const CountProvider = ({children}) => {
 //   🐨 return your context provider with the value assigned to that array and forward all the other props
 //   💰 more specifically, we need the children prop forwarded to the context provider
 
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (context === undefined) {
+    throw new Error('useCount must be used within a CountProvider')
+  }
+  return context
+}
+
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const {count} = React.useContext(CountContext)
+  const [count] = useCount(CountContext)
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const {setCount} = React.useContext(CountContext)
+  const [, setCount] = useCount(CountContext)
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
